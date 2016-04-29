60fb039d642f6c13eff4ddc61cdf9fd57d0f8ffd
JustToBeBetter <li@li.local>
2016-04-29 17:01:13 +0800

    Initial Commit

---
+++ AnimationSet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationSet
+//
+//  Created by 李金柱 on 16/4/29.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationSet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationSet
+//
+//  Created by 李金柱 on 16/4/29.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
